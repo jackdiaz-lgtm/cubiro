339aa3228986bb1bd42de8f260ea705e8a6fc7c6
jackdiaz-lgtm <jack.diaz@eltallerdecocina.com>
2026-07-27 20:29:29 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
-const C='cubiro-v1';
+const C='cubiro-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const req=e.request;
+  const isHTML=req.mode==='navigate'||(req.headers.get('accept')||'').includes('text/html');
+  if(isHTML){
+    e.respondWith(fetch(req).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put('./index.html',cp));return res}).catch(()=>caches.match('./index.html')));
+  }else{
+    e.respondWith(caches.match(req).then(r=>r||fetch(req).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(req,cp));return res}).catch(()=>caches.match('./index.html'))));
+  }
+});
